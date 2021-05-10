294043ef71bdd62c5c7c236bf9a6968981625175
ceberus619 <83043987+ceberus619@users.noreply.github.com>
2021-05-10 01:31:10 -0400

    Create cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "exemplo: 55999000000"
+read -p "Numero -> " nu
+
+loopNET(){
+data=$"msisdn=${nu}&campid=02f76cd1-eead-44ff-88dd-aa69538c7aef&opCode=VV" && si>
+}
+
+loop(){
+clear
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s                               echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s                               echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+echo "enviando mb para ${nu}" && loopNET; sleep 4s
+}
+loop
